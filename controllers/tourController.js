2fca56648596cf483948ea565a1e7ccc1f0e8f37
Chryste Perri <chrysteperri13@gmail.com>
2025-10-23 19:03:09 +0300

    refactor: modularize app by separating routes and controllers

---
+++ controllers/tourController.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+// Parse JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+// Route Handlers
+exports.getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'success',
+    requstedAt: req.requestTime,
+    results: tours.length,
+    data: { tours },
+  });
+};
+
+exports.getTour = (req, res) => {
+  // console.log(req.params);
+  const id = req.params.id * 1; // convert string to number
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Tour not found' });
+  }
+
+  res.status(200).json({ status: 'success', data: { tour } });
+};
+
+exports.createTour = (req, res) => {
+  //console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
+};
+
+exports.updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Inavlid ID' });
+  }
+  res.status(200).json({ status: 'success', data: { tour: '<Updated tour>' } });
+};
+
+exports.deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Inavlid ID' });
+  }
+  res.status(204).json({ status: 'success', data: null });
+};
