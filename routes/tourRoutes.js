07f3cd034fd0b5cfef6ee766cc331ff8b417cff6
Chryste Perri <chrysteperri13@gmail.com>
2025-12-06 19:34:52 +0300

    feat: add alias route to tourRoutes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,12 +3,14 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+router
+  .route('/top-5-tours')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
