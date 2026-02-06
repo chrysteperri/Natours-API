7ef841e1ee7b838aaeadbf6d279ffea0f406b75e
Chryste Perri <chrysteperri13@gmail.com>
2026-02-06 22:54:33 +0300

    feat: update getAllUsers handler to fetch users from database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,18 @@
-// Dummy handlers for users
-exports.getAllUsers = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not yet defined' });
-};
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
 
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  // Send response
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: { users },
+  });
+});
+
+// Dummy handlers for users
 exports.getUser = (req, res) => {
   res
     .status(500)
