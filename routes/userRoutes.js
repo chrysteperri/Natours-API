faeb9c87b06fc80a0351211fc97e215b66c315d5
Chryste Perri <chrysteperri13@gmail.com>
2026-02-23 17:42:19 +0300

    feat: add update password route to user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,8 +6,15 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+
 router.post('/forgot-password', authController.forgotPassword);
 router.patch('/reset-password/:token', authController.resetPassword);
+
+router.patch(
+  '/update-my-password',
+  authController.protect,
+  authController.updatePassword,
+);
 
 router
   .route('/')
