ff98e3b450c65f10d1c7b40d6772cfcfd6114fed
Chryste Perri <chrysteperri13@gmail.com>
2026-03-09 21:57:39 +0300

    feat: add controllers for creating and fetching reviews

---
+++ controllers/reviewController.js
@@ -0,0 +1,25 @@
+const Review = require('../models/reviewModel');
+const catchAsync = require('../utils/catchAsync');
+// const AppError = require('../utils/appError');
+
+// Route Handlers
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: { reviews },
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: { review: newReview },
+  });
+});
