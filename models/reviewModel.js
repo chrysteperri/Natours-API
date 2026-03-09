96512d7ed0429a79766584e3de233e5b24c23c73
Chryste Perri <chrysteperri13@gmail.com>
2026-03-09 22:38:32 +0300

    refactor: remove tour populate in Review model to avoid populate chain

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,11 +32,18 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+// reviewSchema.pre(/^find/, function () {
+//   this.populate({
+//     path: 'tour',
+//     select: 'name',
+//   }).populate({
+//     path: 'user',
+//     select: 'name photo',
+//   });
+// });
+
 reviewSchema.pre(/^find/, function () {
   this.populate({
-    path: 'tour',
-    select: 'name',
-  }).populate({
     path: 'user',
     select: 'name photo',
   });
