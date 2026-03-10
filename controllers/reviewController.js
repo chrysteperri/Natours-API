9a242e5a800e027bb9517f628d011888212e54ac
Chryste Perri <chrysteperri13@gmail.com>
2026-03-10 21:05:47 +0300

    feat: add nested endpoint to get reviews for a specific tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,7 +3,9 @@ const catchAsync = require('../utils/catchAsync');
 
 // Route Handlers
 exports.getAllReviews = catchAsync(async (req, res, next) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId }; // If tourId is present in params, filter reviews by that tour ID
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
