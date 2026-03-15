9cae184a58864d8c6764760c055e0132e9e1208b
Chryste Perri <chrysteperri13@gmail.com>
2026-03-15 20:24:37 +0300

    feat: add getToursWithin controller for geospatial queries

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-// const AppError = require('../utils/appError');
+const AppError = require('../utils/appError');
 
 // Route Handlers
 exports.aliasTopTours = (req, res, next) => {
@@ -69,5 +69,35 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
   res.status(200).json({
     status: 'success',
     data: { plan },
+  });
+});
+
+exports.getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        'Please provide latitude and longitude in the format lat,lng.',
+        400,
+      ),
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: {
+      $geoWithin: { $centerSphere: [[lng, lat], radius] },
+    },
+  });
+
+  // console.log(distance, lat, lng, unit);
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
   });
 });
