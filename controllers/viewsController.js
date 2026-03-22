16143098b35a76479613570973bb8d18ebc4cdc3
Chryste Perri <chrysteperri13@gmail.com>
2026-03-22 21:46:53 +0300

    feat: fetch tour data and pass it to overview and tour views

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,28 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
