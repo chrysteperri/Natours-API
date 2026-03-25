1e4ba3950afe8bebe77b813894a5322ba5dae64a
Chryste Perri <chrysteperri13@gmail.com>
2026-03-25 23:15:27 +0300

    refactor: export login logic and separate DOM handling from login module

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 /* eslint-disable*/
-const login = async (email, password) => {
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -20,10 +20,3 @@ const login = async (email, password) => {
     alert(err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
