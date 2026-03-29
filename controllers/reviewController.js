6748f7c221a5a58dc90dd14ee3e57b84229b1f52
Chryste Perri <chrysteperri13@gmail.com>
2026-03-29 03:34:22 +0300

    fix: validate tour existence before creating reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,8 @@
+const Tour = require('../models/tourModel');
 const Review = require('../models/reviewModel');
+const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-// const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 // Route Handlers
 exports.setTourUserIds = (req, res, next) => {
@@ -9,6 +11,15 @@ exports.setTourUserIds = (req, res, next) => {
   if (!req.body.user) req.body.user = req.user.id;
   next();
 };
+
+exports.validateTourExists = catchAsync(async (req, res, next) => {
+  const tourId = req.body.tour || req.params.tourId;
+  const tour = await Tour.findById(tourId);
+  if (!tour) {
+    return next(new AppError('Cannot create review: Tour does not exist', 404));
+  }
+  next();
+});
 
 exports.getAllReviews = factory.getAll(Review);
 exports.getReview = factory.getOne(Review);
