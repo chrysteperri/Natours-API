ded6798d07797ff2d580862dec218b09d8009170
Chryste Perri <chrysteperri13@gmail.com>
2026-05-17 01:18:12 +0300

    feat: implement Stripe checkout session handling on frontend

---
+++ public/js/stripe.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51THV53RuZ3zOHDzTnWDXC2VHqnU8py2IflLSJJFJPOdIetIS1ofC6ODAlsOAfbvC5bBCAeAamMC5YqzmaY1J3DyK00l7WlOaOq',
+);
+
+export const bookTour = async (tourId) => {
+  try {
+    // Get checkout session from API
+    const session = await axios.get(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
+    );
+    console.log(session);
+
+    // Create checkout form and charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
+
